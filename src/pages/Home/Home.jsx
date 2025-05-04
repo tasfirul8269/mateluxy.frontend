821dd7dcc55e75621ed8595de0503d8f15ab24fb
Tasfirul Haque <adef05675@gmail.com>
2025-05-04 09:46:23 +0600

    SearchBar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,9 +10,9 @@ import NewsAndInsights from "../../components/NewsAndInsights/NewsAndInsights";
 const Home = () => {
   return (
     <div className="mb-10 pt-10">
-      <div className="z-10 md:relative">
+      <div className="md:relative">
         <Banner></Banner>
-        <div className=" md:absolute md:-bottom-14 md:left-1/2 md:transform md:-translate-x-1/2  w-full max-w-4xl px-4 bg-white rounded-[20px] shadow-lg mx-auto -z-40 p-4">
+        <div className=" md:absolute md:-bottom-14 md:left-1/2 md:transform md:-translate-x-1/2  w-full max-w-4xl px-4 bg-white rounded-[20px] shadow-lg mx-auto z-40 p-4">
           <SearchField></SearchField>
         </div>
       </div>
